ac388b6e2fa59da115d30fb87ccee7d5c8f549ac
manjender <mpaul7@gmail.com>
2019-05-11 22:20:49 -0400

    third commit

--- MachineLearning_PythonRecipes/LoadData/LoadData_Pandas.py
+++ MachineLearning_PythonRecipes/LoadData/LoadData_Pandas.py
@@ -5,6 +5,8 @@ def main():
 
     datafile = '/home/mpaul/Dropbox/Professional/dataScientist/dataSets/pima-indians-diabetes.csv'
     names = ['preg', 'plas', 'pres ', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+    # The read_csv() function returns a pandas DataFrame, which enables us to immediately start summarizing
+    # and plotting data.
     data = read_csv(datafile, names=names)
     print(data.shape)
     print(data.head())
