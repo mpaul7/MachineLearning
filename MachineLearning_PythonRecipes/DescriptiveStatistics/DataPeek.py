2b91223bb54b1bbfa06c750b51bf58ca86c47745
manjender <mpaul7@gmail.com>
2019-05-13 13:59:32 -0400

    Peek Data commit

---
+++ MachineLearning_PythonRecipes/DescriptiveStatistics/DataPeek.py
@@ -0,0 +1,14 @@
+from pandas import read_csv
+
+
+def main():
+
+    datafile = '/home/mpaul/Dropbox/Professional/dataScientist/dataSets/pima-indians-diabetes.csv'
+    names = ['preg', 'plas', 'pres ', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+    data = read_csv(datafile, names=names)
+    peek = data.head(20)
+    print(peek)
+
+
+if __name__ == "__main__":
+    main()
--- MachineLearning_PythonRecipes/LoadData/LoadData_Pandas.py
+++ MachineLearning_PythonRecipes/LoadData/LoadData_Pandas.py
@@ -11,6 +11,10 @@ def main():
     print(data.shape)
     print(data.head())
 
+    url = 'https://gist.githubusercontent.com/ktisha/c21e73a1bd1700294ef790c56c8aec1f/raw/819b69b5736821ccee93d05b51de0510bea00294/pima-indians-diabetes.csv'
+    data_url = read_csv(url, names=names)
+    print(data_url.head())
+
 
 if __name__ == "__main__":
     main()
